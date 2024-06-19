712cd56d27ec5f636281f2fa64f98438d6d8e4a7
Kushal Maru <kushalmaru28@gmail.com>
2024-06-19 19:58:56 +0530

    left rotate by one

--- code/arrays/leftRotateByOne.java
+++ code/arrays/leftRotateByOne.java
@@ -1,17 +1,18 @@
-import java.util.Arrays;
 public class leftRotateByOne {
     public static void main(String[] args) {
         int[] arr = {1,2,3,4,5};
-        int[] a = rotate(arr);
-        System.out.print(Arrays.toString(a));
+        rotate(arr);
+        for (int i = 0; i < arr.length; i++) {
+            System.out.println(arr[i]);
+        }
+        
     }
 
-    static int[] rotate(int[] array){
+    static void rotate(int[] array){
         int temp = array[0];
         for (int i = 1; i < array.length; i++) {
             array[i-1] = array[i];
         }
         array[array.length-1] = temp;
-        return array;
     }
 }
