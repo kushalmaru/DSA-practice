4565b254bc5678c50ba7e9a675392c029847ee33
Kushal Maru <kushalmaru28@gmail.com>
2024-07-10 14:19:20 +0530

    Kids With the Greatest Number of Candies

---
+++ code/arrays/kidsWithGrtNumCandies.java
@@ -0,0 +1,28 @@
+// https://leetcode.com/problems/kids-with-the-greatest-number-of-candies/   1431
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class kidsWithGrtNumCandies {
+    public static void main(String[] args) {
+        int[] candies = {2,3,5,1,3};
+        int extraCandies = 3;
+        List<Boolean> soln = new ArrayList<>(kidsWithCandies(candies, extraCandies));
+        System.out.println(soln);
+    }
+
+    static List<Boolean> kidsWithCandies(int[] candies, int extraCandies) {
+        int max = candies[0];
+        List<Boolean> ans = new ArrayList<>();
+        for(int i = 0; i < candies.length; i++){
+            max = Math.max(max, candies[i]);
+            candies[i] = candies[i] + extraCandies;
+            if(candies[i] >= max){
+                ans.add(true);
+            } else{
+                ans.add(false);
+            }
+        }
+        return ans;
+    }
+}
