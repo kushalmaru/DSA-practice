962a896220315b0c0d2da3c60a23e3999e2c2a93
Kushal Maru <kushalmaru28@gmail.com>
2024-07-11 19:41:28 +0530

    To Lowercase

---
+++ code/strings/toLowerCase.java
@@ -0,0 +1,13 @@
+// https://leetcode.com/problems/to-lower-case/   709
+
+public class toLowerCase {
+    public static void main(String[] args) {
+        String s = "Hello";
+        String ans = toLowerCase(s);
+        System.out.println(ans);
+    }
+
+    static String toLowerCase(String s) {
+        return s.toLowerCase();
+    }
+}
