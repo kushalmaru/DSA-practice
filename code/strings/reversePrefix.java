ee7fb8ee661689cc7f42e0ace0ec8b9c32efcf93
Kushal Maru <kushalmaru28@gmail.com>
2024-07-22 20:24:22 +0530

    Reverse Prefix of Word

---
+++ code/strings/reversePrefix.java
@@ -0,0 +1,16 @@
+public class reversePrefix {
+    public static void main(String[] args) {
+        String word = "abcdefd";
+        char ch = 'd';
+        String ans = reversePrefix(word, ch);
+        System.out.println(ans);
+    }
+
+    static String reversePrefix(String word, char ch) {
+        int j = word.indexOf(ch);
+        if (j != -1) {
+            return new StringBuilder(word.substring(0, j + 1)).reverse().toString() + word.substring(j + 1);
+        }
+        return word;
+    }
+}
