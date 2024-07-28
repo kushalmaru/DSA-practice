544caf22734fbbdd679a1d1978b3ece08b42cb07
Kushal Maru <kushalmaru28@gmail.com>
2024-07-28 22:43:07 +0530

    transpose

--- code/arrays/transpose.java
+++ code/arrays/transpose.java
@@ -4,7 +4,7 @@ import java.util.Arrays;
 
 public class transpose {
     public static void main(String[] args) {
-        int[][] matrix = {{1,2,3}, {4,5,6}, {7,8,9}};
+        int[][] matrix = {{1,2,3}, {4,5,6}, {7,8,10}};
         int[][] ans = transpose(matrix);
         System.out.println(Arrays.toString(ans));
     }
