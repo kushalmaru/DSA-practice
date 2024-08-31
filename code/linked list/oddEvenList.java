b6cd64793141aefb8b68762ed06505044a448010
Kushal Maru <kushalmaru28@gmail.com>
2024-08-31 11:46:57 +0530

    Odd Even Linked List

---
+++ code/linked list/oddEvenList.java
@@ -0,0 +1,32 @@
+// https://leetcode.com/problems/odd-even-linked-list/   328
+
+public class oddEvenList {
+    public static void main(String[] args) {
+        
+    }
+
+    private static class ListNode {
+        int val;
+        ListNode next;
+        ListNode() {}
+        ListNode(int val) { this.val = val; }
+        ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+    }
+
+    private static ListNode oddEvenList(ListNode head) {
+        if(head == null || head.next == null){
+            return head;
+        }
+        ListNode odd = head;
+        ListNode even = head.next;
+        ListNode evenHead = head.next;
+        while(odd.next != null && even.next != null){
+            odd.next = odd.next.next;
+            even.next = even.next.next;
+            odd = odd.next;
+            even = even.next;
+        }
+        odd.next = evenHead;
+        return head;
+    }
+}
