d92ccbc17b5492045b80c8e113e8cd97d013a9a5
Kushal Maru <kushalmaru28@gmail.com>
2024-09-01 17:02:57 +0530

    Palindrome Linked List

---
+++ code/linked list/isPalindrome.java
@@ -0,0 +1,48 @@
+// https://leetcode.com/problems/palindrome-linked-list/   234
+
+public class isPalindrome {
+    public static void main(String[] args) {
+        
+    }
+
+    private static class ListNode {
+        int val;
+        ListNode next;
+        ListNode() {}
+        ListNode(int val) { this.val = val; }
+        ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+    }
+
+    private static boolean isPalindrome(ListNode head) {
+        ListNode slow = head;
+        ListNode fast = head;
+        while(fast.next != null && fast.next.next != null){
+            slow = slow.next;
+            fast = fast.next.next;
+        }
+        ListNode rev = reverse(slow.next);
+        ListNode first = head;
+        ListNode second = rev;
+
+        while(second != null){
+            if(first.val != second.val){
+                return false;
+            }
+            first = first.next;
+            second = second.next;
+        }
+        return true;
+    }
+
+
+    private static ListNode reverse(ListNode head){
+        if(head == null || head.next == null){
+            return head;
+        }
+        ListNode newHead = reverse(head.next);
+        ListNode front = head.next;
+        front.next = head;
+        head.next = null;
+        return newHead;
+    }
+}
