2520282b12b24e636b2101408b5cf7edc577949b
Kushal Maru <kushalmaru28@gmail.com>
2024-09-01 13:38:18 +0530

    Reverse Linked List

---
+++ code/linked list/reverseList.java
@@ -0,0 +1,27 @@
+// https://leetcode.com/problems/reverse-linked-list/   206
+
+public class reverseList {
+    public static void main(String[] args) {
+        
+    }
+
+    private static class ListNode {
+        int val;
+        ListNode next;
+        ListNode() {}
+        ListNode(int val) { this.val = val; }
+        ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+    }
+
+    private static ListNode reverseList(ListNode head) {
+        ListNode temp = head;
+        ListNode prev = null;
+        while(temp != null){
+            ListNode front = temp.next;
+            temp.next = prev;
+            prev = temp;
+            temp = front;
+        }
+        return prev;
+    }
+}
