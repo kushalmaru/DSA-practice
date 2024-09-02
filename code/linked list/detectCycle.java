c2386bed0e2134f6493ccdd6506445828657e761
Kushal Maru <kushalmaru28@gmail.com>
2024-09-02 16:03:11 +0530

    Linked List Cycle II

---
+++ code/linked list/detectCycle.java
@@ -0,0 +1,35 @@
+// https://leetcode.com/problems/linked-list-cycle-ii/   142
+
+public class detectCycle {
+    public static void main(String[] args) {
+        
+    }
+
+    private static class ListNode {
+        int val;
+        ListNode next;
+        ListNode() {}
+        ListNode(int val) { this.val = val; }
+        ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+    }
+
+    private static ListNode detectCycle(ListNode head) {
+        ListNode slow = head;
+        ListNode fast = head;
+
+        while(fast != null && fast.next != null){
+            slow = slow.next;
+            fast = fast.next.next;
+
+            if(slow == fast){
+                slow = head;
+                while(slow != fast){
+                    slow = slow.next;
+                    fast = fast.next;
+                }
+                return slow;
+            }
+        }
+        return null;
+    }
+}
