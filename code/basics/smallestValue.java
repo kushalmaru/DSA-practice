d78880b8b389a61defbc39c3dff843c01a6e67a6
Kushal Maru <kushalmaru28@gmail.com>
2024-12-30 21:16:31 +0530

    Smallest Value After Replacing With Sum of Prime Factors

---
+++ code/basics/smallestValue.java
@@ -0,0 +1,27 @@
+public class smallestValue {
+    public static void main(String[] args) {
+        
+    }
+
+    static int smallestValue(int n) {
+        while(true){
+            int temp = n;
+            int sum = 0;
+            for(int i = 2; i * i <= temp; i++){
+                while(temp % i == 0){
+                    sum = sum + i;
+                    temp = temp / i;
+                }
+            }
+            if(temp != 1){
+                sum = sum + temp;
+            }
+
+            if(sum == n){
+                break;
+            }
+            n = sum;
+        }
+        return n;
+    }
+}
